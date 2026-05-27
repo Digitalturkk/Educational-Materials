0ac5e6d774a64501d9063237d90f1c879837ee5e
ITtsuki <155392795+Digitalturkk@users.noreply.github.com>
2026-05-28 00:03:39 +0400

    Add camera simulation using OpenCV

--- Cyber Security/Camera Simulator.py
+++ Cyber Security/Camera Simulator.py
@@ -1,1 +1,17 @@
+import cv2
 
+cam = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cam.read()
+
+    if not ret:
+        break
+
+    cv2.imshow("Camera Demo", frame)
+
+    if cv2.waitKey(1) == 27:
+        break
+
+cam.release()
+cv2.destroyAllWindows()
