da19e79906996ddd6df7cc80b6af16b7fb550536
ITtsuki <155392795+Digitalturkk@users.noreply.github.com>
2026-05-27 23:04:11 +0400

    Add keylogger implementation in KeyLogger.py
    Implement basic keylogger functionality using pynput.

--- Cyber Security/KeyLogger.py
+++ Cyber Security/KeyLogger.py
@@ -1,1 +1,19 @@
+from pynput import keyboard
 
+pressed = set() #you may also use list
+
+def on_press(key):
+    pressed.add(str(key)) #if you have choosed list over set, change it to .append
+    print("Сейчас нажаты:", pressed)
+
+def on_release(key):
+    pressed.discard(str(key)) #the same here .discard -> .remove
+
+    if key == keyboard.Key.esc: #using escep as button to stop our keylogger
+        return False
+
+with keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release
+) as listener:
+    listener.join()
